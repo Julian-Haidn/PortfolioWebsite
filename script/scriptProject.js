5610bcedde7bc4589f64f0947c0a26bfef829be8
Julian Haidn <julian.haidn@gmail.com>
2023-03-29 16:28:46 +0200

    Project Selector Frontend finished

--- script/scriptProject.js
+++ script/scriptProject.js
@@ -1,12 +1,13 @@
 // Select all elements with the class "menu-selection"
 const projectSelections = document.querySelectorAll('.menu-selection');
+const menuContentH2 =     document.querySelector('.menu-content h2');
+const menuContentP =      document.querySelector('.menu-content p');
+const projectSelector =   document.querySelector('.project-selections');
 
-const menuContentH2 = document.querySelector('.menu-content h2');
-const menuContentP = document.querySelector('.menu-content p');
-const projectSelector = document.querySelector('.project-selections');
 
-
-// Loop through each selected element and change its text content
+/** Loop through each selected element of div "project-selections"
+* by click change Project Type and Content
+*/
 projectSelections.forEach((item, index) => {
     item.onclick = function() {
       
@@ -19,18 +20,16 @@ projectSelections.forEach((item, index) => {
         }
       
         //update Content
-        console.log("clicked: " + index);
         updateProject(index);
-      
     }
 });
 
-/** Function updateProject
-* updates the submenu of Project
-* updates the Content of the submenu
-* updates the Project Selection
 
-*/
+/** Function updateProject
+ * updates the submenu of Project
+ * updates the Content of the submenu
+ * updates the Project Selection
+ */
 function updateProject(index){
   document.documentElement.style.setProperty('--color-accent-current', "var(--color-accent-" + index + ")");
   
@@ -42,11 +41,61 @@ function updateProject(index){
   while (projectSelector.firstChild) {
     projectSelector.removeChild(projectSelector.firstChild);
   }
-  // Loop through all Projects in Projecttype
-  projectContentGer[index].projects.forEach(prj => {
-
-      const newDiv = document.createElement('div');
-      newDiv.className = 'p-selection';
-      projectSelector.appendChild(newDiv);
+  // Add Selections (p-selection -> (cursor-current, cursor-selection))
+  projectContentGer[index].projects.forEach((prj, ind ) => {
+    
+    const newDiv = document.createElement('div');
+    newDiv.className = 'p-selection';
+    newDiv.innerHTML = '<div class="cursor-current"></div><div class="cursor-selection"></div>';
+    projectSelector.appendChild(newDiv);
+    
   });
+  updateSelectedProject(0);
+  mouseeventsProjectSelecter();
 }
+
+
+/** 
+ * given Param "objectIndex"
+ * add the class "selectedProject" only to Param
+ */
+function updateSelectedProject(objectIndex){
+  // delet all 'selectedProject'
+  for (var k = 0; k < projectSelector.children.length; k++) {
+    projectSelector.children[k].classList.remove('selectedProject');
+  }
+  //add
+  projectSelector.children[objectIndex].classList.add('selectedProject');
+}
+
+
+/** Function mouseeventsProjectSelecter
+ * adds mouseover event to Project Seleter
+ * adds click event to Project Seleter
+ */
+function mouseeventsProjectSelecter(){
+  Array.from(projectSelector.children).forEach((child, childIndex) => {
+    var x = 0;
+    
+    //add mouseover
+    child.addEventListener('mousemove', function (event) {
+      let bnds = event.target.getBoundingClientRect();
+      x =        event.clientX - bnds.left; // the value of x
+      //update css Var
+      document.documentElement.style.setProperty('--cursor-selection', (x-5) + "px");
+    });
+    
+    //add onClick
+    child.addEventListener('mousedown', function (event) {
+      document.documentElement.style.setProperty('--cursor-current', (x-5) + "px");
+      updateSelectedProject(childIndex);
+      
+      //set Video to this Position
+    });
+    
+    
+  });
+
+}
+
+
